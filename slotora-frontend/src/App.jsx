6ccc68eb297166655bd8ed31b6ddf0fc45be5ccc
athishrs <athishrs02@gmail.com>
2026-06-05 11:10:09 -0400

    Front-end setuo

---
+++ slotora-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-[#FDFAF4] min-h-screen">
+      <h1 className="text-3xl font-bold text-[#3E342E]">Slotora</h1>
+    </div>
+  )
+}
+
+export default App
